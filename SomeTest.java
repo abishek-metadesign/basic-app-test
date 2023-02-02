445a35a9a56bc3c6b92a6a334f50f91937ee93d1
abishek <abishek.dulal@metadesignsolutions.co.uk>
2023-02-02 19:17:30 +0545

    fixed the tests

--- SomeTest.java
+++ SomeTest.java
@@ -2,10 +2,15 @@ package com.sample.basicapp.external;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
 
 public class SomeTest {
 
     @Test
+    @Tags({@Tag("position=senior"),
+            @Tag("points=3"),
+            @Tag("name=This_is_a_test")})
     public void correctText(){
         Assertions.assertEquals(1,1);
     }
